2b1a921081883a1fd6bfd88d2f5897113a42d024
Chanindu1117 <chanindukannangara1117@gmail.com>
2025-11-09 16:48:32 +0530

    Create interactive number guessing game with user input

---
+++ src/Number_Guessing_Game/Main.java
@@ -0,0 +1,39 @@
+package Number_Guessing_Game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int attempts=0;
+        int min=1;
+        int max=100;
+        int randomNumber=random.nextInt(min,max+1);
+
+        System.out.println("Number Guessing Game");
+        System.out.printf("Guess a number between %d-%d\n",min,max);
+        do{
+            System.out.print("Enter a guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if(guess<randomNumber){
+                System.out.println("Too LOW! Try again!");
+            }
+            else if(guess>randomNumber){
+                System.out.println("Too HIGH! Try again!");
+            }
+            else{
+                System.out.println("Correct! The number was " + randomNumber);
+                System.out.println("# of attempts: " + attempts);
+            }
+
+        }while(guess!=randomNumber);
+
+        scanner.close();
+    }
+}
